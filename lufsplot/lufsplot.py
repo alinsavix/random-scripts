40de4b78bde4b047b78d0bf7cd0bd0baf63559f0
TDV Alinsa <alinsa-github@lupine.org>
2022-09-18 15:48:03 -0700

    lufsplot: mostly feature complete
    Can enable or disable various graphs, choose to write graphs to disk, etc.
    
    usage: lufsplot.py [-h] [-o OUTFILE] [--noninteractive] [-w] [--momentary]
                       [--short] [--integrated] [--lra] [--clipping]
                       file
    
    Generate a BS.1770-based loudness graph for a file w/ audio
    
    positional arguments:
      file                  file for which to analyze loudness
    
    optional arguments:
      -h, --help            show this help message and exit
      -o OUTFILE, --outfile OUTFILE
                            filename to output generated graph to (defaults based
                            on input name)
      --noninteractive      don't show interactive graph (implies -w)
      -w, --write           write graph to file on disk
      --momentary, --no-momentary
                            generate plots for momentary (400ms window) loudness
                            (default: no)
      --short, --no-short   generate plots for short (3s window) loudness
                            (default: yes)
      --integrated, --no-integrated
                            generate plots for integrated (full-length) loudness
                            (default: yes)
      --lra, --no-lra       generate plot for loudness range (default: yes)
      --clipping, --no-clipping
                            show where true peak is higher than -1.0dbFS (default:
                            yes)

--- lufsplot/lufsplot.py
+++ lufsplot/lufsplot.py
@@ -1,21 +1,19 @@
 #!/usr/bin/env python3
 import argparse
-import dataclasses
-import logging as lg
+# import logging as lg
 import re
 import subprocess
 import sys
 from enum import IntEnum
 from pathlib import Path
-from typing import List, Optional, Tuple
+from typing import (Any, Dict, List, Optional, Sequence, Set, Text, Tuple,
+                    Type, Union)
 
 import matplotlib.pyplot as plt
-import numpy
 import numpy as np
 import numpy.typing as npt
-from tdvutil.argparse import CheckFile, NegateAction
 
-# ffmpeg -i thing.mp4 -af ebur128=peak=true -ar 4800 -f null -
+# ffmpeg -i thing.mp4 -af ebur128=peak=true -f null -
 #
 # output (note: actual output is not linewrapped):
 # [Parsed_ebur128_0 @ 00000132f15e35c0] t: 763.8
@@ -82,66 +80,28 @@ re_ffmpeg_summary = re.compile(r"""
 """, re.VERBOSE | re.IGNORECASE)
 
 
-def parse_arguments(argv):
-    parser = argparse.ArgumentParser(
-        description='Generate a BS.1770-based loudness graph for a file w/ audio'
-    )
-
-    # parser.add_argument(
-    #     "--debug",
-    #     action='store_const',
-    #     const=True,
-    #     default=False,
-    #     # help="Enable debugging",
-    # )
-
-    # parser.add_argument(
-    #     "-o",
-    #     "--out",
-    #     type=Path,
-    #     default=None,
-    #     action=CheckFile(extensions={'png'}),
-    #     help="filename to output generated graph to",
-    # )
-
-    # parser.add_argument(
-    #     "--noninteractive",
-    #     default=True,
-    #     action='store_false',
-    #     dest="interactive",
-    #     help="don't show interactive graph",
-    # )
-
-    # positional arguments
-    parser.add_argument(
-        "file",
-        type=Path,
-        action=CheckFile(must_exist=True),
-        help="file for which to analyze loudness",
-    )
-
-    parsed_args = parser.parse_args(argv)
-    print(parsed_args)
-
-    return parsed_args
+# Theoretically you can name your fields in a multi-dimensional array, but I
+# wasn't smart enough to figure out how to make that work, so we'll just number
+# them instead (and use an enum to sanely identify them)
+class Fields(IntEnum):
+    TIME = 0
+    TARGET = 1
+    MOMENTARY = 2
+    SHORT = 3
+    INTEGRATED = 4
+    LRA = 5
+    FTPK = 6
+    TPK = 7
 
 
-def main(argv: List[str]) -> int:
-    # args = parse_arguments(argv)
-    #
-    # loglevel = "DEBUG" if args.debug else "INFO"
-
-    args = parse_arguments(argv[1:])
-
-    # loglevel = "INFO"
-    # LOG_FORMAT = "[%(filename)s:%(lineno)s:%(funcName)s] (%(name)s) %(message)s"
-    # lg.basicConfig(level=loglevel, format=LOG_FORMAT)
-
-    # log = lg.getLogger()
+# Can't quiiiiite type the multi-dimensional numpy array correctly, alas
+def gen_loudness(file: Path) -> Tuple[npt.NDArray[Any], Dict[str, float]]:
+    # Create python lists now, then convert to a numpy array at the end, for
+    # much better performance.
+    li: List[List[float]] = []
 
     proc = subprocess.Popen(
-        ["ffmpeg", "-i", str(args.file), "-af", "ebur128=peak=true",
-            "-ar", "4800", "-f", "null", "-"],
+        ["ffmpeg", "-i", str(file), "-af", "ebur128=peak=true", "-f", "null", "-"],
         shell=False,
         stdin=subprocess.DEVNULL,
         stdout=subprocess.PIPE,
@@ -150,33 +110,16 @@ def main(argv: List[str]) -> int:
     )
     assert proc.stdout is not None
 
-
-    class Fields(IntEnum):
-        TIME = 0
-        TARGET = 1
-        MOMENTARY = 2
-        SHORT = 3
-        INTEGRATED = 4
-        LRA = 5
-        FTPK = 6
-        TPK = 7
-
-
-    # loudness: List[LoudnessInfo] = []
-    # plt.figure(figsize=(14, 5))
-    # plt.ion()
-    # hl, = plt.plot([], [])
-    # plt.show()
-    # li = LoudnessInfo()
-    li: List[List[float]] = []
-
+    # read and parse the results, a line at a time
+    # FIXME: if we use split() instead of regexes here, how does that change performance?
     while proc.stdout.readable():
         line = proc.stdout.readline()
+
         if not line:
             break
 
+        # if we hit the summary block, we're mostly done, so bail
         if " Summary:" in line:
-            # print("starting summary")
             break
 
         # print(line)
@@ -186,8 +129,16 @@ def main(argv: List[str]) -> int:
             # print(f"DEBUG: SKIPPING LINE: {line}")
             continue
 
+        # Until we get a full 'short' chunk of audio, many of our stats are
+        # going to be super wonky, so we're just going to completely skip that
+        # data, since we won't have any pathological cases where it actually
+        # matters
+        if float(m.group("time")) <= 3.1:
+            continue
 
-
+        # Make a single datapoint, and append it to our collection. Pity that
+        # you can't specify data types as part of the regex itself so that
+        # you don't have to convert each thing here...
         one = [
             float(m.group("time")),
             float(m.group("target")),
@@ -200,10 +151,9 @@ def main(argv: List[str]) -> int:
         ]
         li.append(one)
 
-
     # If we got here, we either ran out of data, or we hit the summary
     # block, so do our best to read whatever summary info if it exists:
-    summary = {}
+    summary: Dict[str, float] = {}
     while proc.stdout.readable():
         line = proc.stdout.readline()
 
@@ -219,7 +169,13 @@ def main(argv: List[str]) -> int:
 
 
     # We've gathered all the data, put it in a useful form
-    lind = np.array(li)
+    lind: npt.NDArray[Any] = np.array(li)  # type: ignore
+
+    return lind, summary
+
+
+# Most of the matplotlib stuff have shitty typing support :(
+def gen_graph(lind: npt.NDArray[Any], summary: Dict[str, float], args: argparse.Namespace):
     T = lind[:, Fields.TIME]
     momentary = lind[:, Fields.MOMENTARY]
     short = lind[:, Fields.SHORT]
@@ -227,77 +183,227 @@ def main(argv: List[str]) -> int:
     lra = lind[:, Fields.LRA]
     ftpk = lind[:, Fields.FTPK]
 
-    # plt.figure(figsize=(14, 8))
-    fig, ax1 = plt.subplots(figsize=(19.20, 10.80), dpi=72,
-                            linewidth=0.1, tight_layout=True)
-    ax2 = ax1.twinx()
-
-    ax1.set_ylim(ymin=-60, ymax=6)
-    ax2.set_ylim(ymin=0, ymax=31)
+    _, ax1 = plt.subplots(figsize=(19.20, 10.80), dpi=72,
+                          linewidth=0.1, tight_layout=True)
 
     ax1.set_xlabel("seconds")
     ax1.set_xticks(list(range(0, int(np.max(T)), 30)))
 
+    ax1.set_ylim(ymin=-60, ymax=6)
     ax1.set_ylabel("Loudness (LUFS)")
     ax1.set_yticks(list(range(-60, 7, 6)))
     ax1.set_yticks(list(range(-57, 4, 6)), minor=True)
     ax1.grid(True, linestyle="dotted")
 
     ax1.axhline(y=-14.0, color='orange',
-                label="_Youtube Integrated Limit", linewidth=3, alpha=1.0)
-    ax1.annotate("Youtube Integrated Limit (-14.0 LUFS)", (0, -13.9), fontsize=14)
-
-    ax2.set_ylabel("Loudness Range (LU)")
-    ax2.set_yticks(list(range(0, 31, 2)))
+                label="_Youtube Integrated Target", linewidth=3, alpha=1.0)
+    ax1.annotate("Youtube Integrated Target (-14.0 LUFS)", (0, -13.9), fontsize=14)
 
 
-    ax1.plot(T, np.ma.masked_where(momentary <= -120.7, momentary),
-             label="Momentary", color='r', linewidth=0.5)
-    ax1.plot(T, np.ma.masked_where(short <= -120.7, short),
-             label="Short", color='b', linewidth=1)
-    ax1.plot(T, np.ma.masked_where(integrated <= -70.0, integrated),
-             label="Integrated", color='b', linewidth=2)
-    ax1.plot(T, np.ma.masked_where(ftpk < -1.0, ftpk), label="Clipping", color='r', linewidth=3)
+    if args.momentary:
+        ax1.plot(T, np.ma.masked_where(momentary <= -120.7, momentary),
+                 label="Momentary", color='m', linewidth=0.5)
 
+    if args.short:
+        ax1.plot(T, np.ma.masked_where(short <= -120.7, short),
+                 label="Short", color='b', linewidth=0.75)
 
+    if args.integrated:
+        ax1.plot(T, np.ma.masked_where(integrated <= -70.0, integrated),
+                 label="Integrated", color='b', linewidth=2)
 
-    ax2.plot(T, lra, label="Loudness Range", color='g', linewidth=1.5)
+    if args.clipping:
+        ax1.plot(T, np.ma.masked_where(ftpk < -1.0, ftpk),
+                 label="Clipping", color='r', linewidth=3)
 
     ax1.legend(loc='upper left', shadow=True, fontsize='large')
-    ax2.legend(loc='upper right', shadow=True, fontsize='large')
 
     if summary["LRA low"] and summary["LRA high"]:
         ax1.axhspan(summary["LRA low"], summary["LRA high"], color='g', alpha=.1)
 
-    # fig.align_labels()
+    if summary["I"]:
+        ax1.plot(T[-1:], summary["I"], label="_Integrated Final",
+                 color='b', marker='o', markersize=7)
+        ax1.annotate(str(summary["I"]), (T[-1], summary["I"] + 0.5), fontsize=18)
 
-    plt.show()
+    if args.lra:
+        ax2 = ax1.twinx()
+        ax2.set_ylim(ymin=0, ymax=31)
+        ax2.set_ylabel("Loudness Range (LU)")
+        ax2.set_yticks(list(range(0, 31, 2)))
+        ax2.plot(T, lra, label="Loudness Range", color='g', linewidth=1.5)
+        ax2.legend(loc='upper right', shadow=True, fontsize='large')
+
+    if args.write_graph:
+        plt.savefig(args.outfile)
+
+    if args.interactive:
+        plt.show()
+
+    # print(summary)
 
     return 0
 
-    # y_vals_masked = np.ma.masked_where(np.asfarray(li.momentary) < -25, li.momentary)
-    # y_vals_masked = np.ma.masked_where(M < -25, M)
 
-    # plt.plot(li.time, y_vals_masked)
-    # plt.plot(T, y_vals_masked, label="Momentary", color='r', linewidth=1)
-    # plt.plot(T, lind[:, IDX_SHORT], label="Short", color='b', linewidth=1)
-    # plt.plot(T, lind[:, IDX_INTEGRATED], label="Integrated", color='b', linewidth=2)
-    # plt.show()
-    # print(lind.shape)
-    # print(np.obj2sctype(lind))
+def CheckFile(extensions: Optional[Set[str]] = None, must_exist: bool = False) -> Type[argparse.Action]:
+    class Act(argparse.Action):
+        def __call__(self, parser: argparse.ArgumentParser, namespace: argparse.Namespace,
+                     values: Any, option_string: Optional[Text] = "") -> None:
+            if not isinstance(values, Path):
+                parser.error(
+                    f"CheckFile called but argument is {type(values)} and not a pathlib path? (bug)")
+
+            if extensions:
+                ext = values.suffix[1:]
+                if ext not in extensions:
+                    # option_string = '({})'.format(option_string) if option_string else ''
+                    parser.error(f"file '{values}' doesn't end with one of {extensions}")
+
+            if must_exist:
+                if not values.exists():
+                    parser.error(f"file '{values}' does not exist")
+
+            setattr(namespace, self.dest, values)
+
+    return Act
+
+# FIXME: I think there's some way to expose an 'nargs' value so that we don't
+# have to explicity specify "nargs=0" when using this?
+class NegateAction(argparse.Action):
+    def __call__(self, parser: argparse.ArgumentParser, namespace: argparse.Namespace,
+                 _values: Union[Text, Sequence[Any], None], option_string: Optional[Text] = "") -> None:
+        if option_string is None:
+            parser.error("NegateAction can only be used with non-positional arguments")
+
+        # assert option_string is not None
+        setattr(namespace, self.dest, option_string[2:4] != 'no')
+
+def parse_arguments(argv: List[str]):
+    parser = argparse.ArgumentParser(
+        description="Generate a BS.1770-based loudness graph for a file w/ audio",
+        allow_abbrev=True,
+    )
+
+    # parser.add_argument(
+    #     "--debug",
+    #     action='store_const',
+    #     const=True,
+    #     default=False,
+    #     # help="Enable debugging",
+    # )
+
+    parser.add_argument(
+        "-o",
+        "--outfile",
+        type=Path,
+        default=None,
+        action=CheckFile(extensions={'png'}),
+        help="filename to output generated graph to (defaults based on input name)",
+    )
+
+    parser.add_argument(
+        "--noninteractive",
+        default=True,
+        action='store_false',
+        dest="interactive",
+        help="don't show interactive graph (implies -w)",
+    )
+
+    parser.add_argument(
+        "-w",
+        "--write",
+        default=False,
+        action='store_true',
+        dest="write_graph",
+        help="write graph to file on disk",
+    )
+
+    parser.add_argument(
+        "--momentary",
+        "--no-momentary",
+        default=False,
+        action=NegateAction,
+        nargs=0,
+        help="generate plots for momentary (400ms window) loudness (default: no)",
+    )
+
+    parser.add_argument(
+        "--short",
+        "--no-short",
+        default=True,
+        action=NegateAction,
+        nargs=0,
+        help="generate plots for short (3s window) loudness (default: yes)",
+    )
+
+    parser.add_argument(
+        "--integrated",
+        "--no-integrated",
+        default=True,
+        action=NegateAction,
+        nargs=0,
+        help="generate plots for integrated (full-length) loudness (default: yes)",
+    )
+
+    parser.add_argument(
+        "--lra",
+        "--no-lra",
+        default=True,
+        action=NegateAction,
+        nargs=0,
+        help="generate plot for loudness range (default: yes)",
+    )
+
+    parser.add_argument(
+        "--clipping",
+        "--no-clipping",
+        default=True,
+        action=NegateAction,
+        nargs=0,
+        help="show where true peak is higher than -1.0dbFS (default: yes)",
+    )
+
+    # positional arguments
+    parser.add_argument(
+        "file",
+        type=Path,
+        action=CheckFile(must_exist=True),
+        help="file for which to analyze loudness",
+    )
+
+    parsed_args = parser.parse_args(argv)
+
+    if parsed_args.write_graph and parsed_args.outfile is None:
+        parsed_args.outfile = parsed_args.file.with_suffix(".loudness.png")
+
+    # make sure we enable writing the graph if the user specifies a filename
+    if parsed_args.outfile:
+        parsed_args.write_graph = True
+
+    if not parsed_args.interactive:
+        parsed_args.write_graph = True
+
+    return parsed_args
+
+
+def main(argv: List[str]) -> int:
+    args = parse_arguments(argv[1:])
+
+    # loglevel = "DEBUG" if args.debug else "INFO"
+    # loglevel = "INFO"
+    # LOG_FORMAT = "[%(filename)s:%(lineno)s:%(funcName)s] (%(name)s) %(message)s"
+    # lg.basicConfig(level=loglevel, format=LOG_FORMAT)
+
+    # log = lg.getLogger()
+
+    lind, summary = gen_loudness(args.file)
+    gen_graph(lind, summary, args)
+
+    return 0
 
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
-
-
-
-
-
-
-
-
-
 
 
 # common loudness ranges:
@@ -312,29 +418,74 @@ if __name__ == "__main__":
 #
 #
 
-# Values to look out for
+# Loudness ranges:
 #
 # A low LRA low reading indicates a track with narrow dynamic range and a high
 # LRA indicates a track with wide dynamic range.
 #
-# So it is not unusual for a techno track to have an LRA of 3 LU (Loudness
-# range is measured in LU = Loudness Units) and a classical music track can
-# have as much as 15 LU or more.
+# So it is not unusual for a techno track to have an LRA of 3LU and a classical
+# music track can have as much as 15 LU or more.
 #
 # Depending on genre an LRA of around 9 - 10 LU is considered a good indication
 # of a healthy dynamic range.
+
+
+# Peak to Loudness Ratio (PLR)  [not implemented]
 #
-# The LRA is not typically useful for music shorter than 30 seconds.
-#
-# Dynamic Range Meter
-#
-# A dynamic range meter will give a measurement called a Peak to Loudness Ratio (PLR)
-#
-# The PLR is the difference between the highest True Peak and the integrated
-# loudness. A high PLR indicates wide dynamic range. ("crest factor")
+# A dynamic range meter may give a measurement called a Peak to Loudness
+# Ratio (PLR).The PLR is the difference between the highest True Peak and the
+# integrated loudness. A high PLR indicates wide dynamic range. ("crest factor")
 #
 # There is also a measure called the PSR (peak to short term ratio) which does
 # the same measurement, but based off a three second window and short term
 # loudness. PSR measures the dynamics of the last three seconds and a high
 # number indicates well preserved transients, a small PSR indicates strong
 # limiting or compression.
+
+
+# Some suggested limits for various online services:
+#
+# Youtube:
+#   short:  -10 / -18
+#   tpk: -1.0
+#   target: -14.0
+#
+# amazon music:
+#   short:  -10 / -18
+#   tpk: -2.0
+#   target: -14.0
+#
+# spotify:
+#   short: -10 / -18
+#   tpk: -1
+#   target: -14.0
+#
+# apple music:
+#   short: -12 / -20
+#   tpk: -1
+#   target: -16
+#
+# soundcloud:
+#   short: -10 / -18
+#   tpk: -1
+#   target: -14
+#
+# tidal:
+#   short: -10 / -18
+#   tpk: -1
+#   target: -14
+#
+# ATSC:
+#   use LM1 / 10s short window
+#   short:  -16 / -32
+#   tpk: -2
+#   target: -24
+#
+# •EBU uses foreground audio as the loudness anchor.
+# •LM1 measures and averages loudness across the whole program.
+
+
+# EBU TECH 3343 says:
+# Momentary - Loudness (M) -- time window: 400ms
+# Short Term - Loudness (S) -- time window: 3s
+# Integrated - Loudness (I) -- time window: all
