039d1b44d2fef103ef5340b734af6ea919a9fa06
TDV Alinsa <alinsa-github@lupine.org>
2023-05-03 12:01:29 -0700

    makesources: make OBS alert sources from a config file

---
+++ makesources/makesources.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+import csv
+import msvcrt
+import os
+import sys
+import time
+from pathlib import Path
+from typing import Any, Dict, List, Optional, cast
+
+from obswebsocket import obsws, requests
+from tdvutil import alintrospect, ppretty
+
+CHEERS_SCENE = "CHEERS"
+
+# global counters (yeah, this is laaaaame)
+counters = {
+    "ok": 0,
+    "exists": 0,
+    "missing": 0,
+    "failure": 0,
+}
+
+def parse_bool(s: str):
+    if not s or s.lower() == "false":
+        return False
+
+    if s.lower() == "true":
+        return True
+
+    # else we don't know what it is
+    print(f"WARNING: Unknown bool value {s}")
+    return False
+
+
+def scene_by_name(ws: Any, name: str) -> Optional[int]:
+    scenes = ws.call(requests.GetSceneList()).getScenes()
+    for s in scenes:
+        if name == s['sceneName']:
+            return s['sceneIndex']
+
+    return None
+
+
+def media_source_by_name(ws: Any, name: str) -> Optional[Dict[str, Any]]:
+    ret = ws.call(requests.GetInputSettings(inputName=name))
+    if not ret.status:
+        return None
+
+    # else
+    return ret.getInputSettings()
+
+
+def create_media_source_from_cfg(ws: Any, scene: str, name: str, cfg: Dict[str, Any]) -> Optional[bool]:
+    res = ws.call(
+        requests.CreateInput(sceneName=scene, inputName=name,
+                             inputKind="ffmpeg_source", sceneItemEnabled=False, inputSettings=cfg))
+    if not res.status:
+        # print(f"WARING: failed to create media source '{name}'")
+        return None
+
+    id = res.getSceneItemId()
+    # print(f"OK: Created media source '{name}' as id {id}")
+    return id
+
+
+# Create an alert source, and print the result (yeah, the print isn't great)
+def create_alert(ws: Any, scene: str, name: str, file=Path, loop=False) -> Optional[int]:
+    input_cfg = {
+        'is_local_file': True,
+        'local_file': str(file.resolve()),
+        'looping': loop,
+        'restart_on_activate': True,
+        'hw_decode': True,
+        'clear_on_media_end': True,
+        'linear_alpha': False,
+        'speed_percent': 100,
+        'close_when_inactive': False,
+    }
+
+    global counters
+    src = media_source_by_name(ws, name)
+    if src is not None:
+        print("EXISTS (not touching)")
+        counters["exists"] += 1
+        return None
+    else:
+        if not file.exists():
+            print(f"FAILURE (missing video file '{file}')")
+            counters["missing"] += 1
+            return None
+
+        newsrc = create_media_source_from_cfg(ws, scene, name, input_cfg)
+        if newsrc is None:
+            print("FAILURE (unknown)")
+            counters["failure"] += 1
+            return None
+        else:
+            print(f"created with id {newsrc}")
+            counters["ok"] += 1
+            return newsrc
+
+
+def set_source_transform(ws: Any, scene: str, id: str, transform: Dict[str, Any]) -> bool:
+    res = ws.call(
+        requests.SetSceneItemTransform(sceneName=scene, sceneItemId=id, sceneItemTransform=transform))
+    return res.status
+
+
+def set_monitoring_all(ws: Any, source: str) -> bool:
+    res = ws.call(requests.SetInputAudioMonitorType(inputName=source,
+                  monitorType='OBS_MONITORING_TYPE_MONITOR_AND_OUTPUT'))
+    return res.status
+
+
+def set_monitor_only(ws: Any, source: str) -> bool:
+    res = ws.call(requests.SetInputAudioMonitorType(inputName=source,
+                  monitorType='OBS_MONITORING_TYPE_MONITOR_ONLY'))
+    return res.status
+
+
+def main():
+    if getattr(sys, 'frozen', False):
+        # os.chdir(sys._MEIPASS)
+        os.chdir(os.path.dirname(sys.executable))
+    elif __file__:
+        os.chdir(os.path.dirname(__file__))
+
+    ws = obsws("localhost", 4455, "secret")
+    ws.connect()
+    ver = ws.call(requests.GetVersion()).getObsVersion()
+    print(f"INFO: Connected to OBS, version {ver}")
+
+    sceneidx = scene_by_name(ws, CHEERS_SCENE)
+
+    if sceneidx is not None:
+        print(f"INFO: Found existing scene '{CHEERS_SCENE}' at index {sceneidx}, using that")
+    else:
+        print(f"INFO: Couldn't find a scene '{CHEERS_SCENE}', creating")
+        x = ws.call(requests.CreateScene(sceneName=CHEERS_SCENE))
+        if not x.status:
+            print(f"ERROR: Failed to create scene '{CHEERS_SCENE}'")
+            sys.exit(1)
+
+
+    print("CREATING SOURCES:")
+
+    with open("botconfig.csv", 'r') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if row["Scene"] != CHEERS_SCENE:
+                continue
+
+            # otherwise, create it
+            print(f"  {row['Source']}: ", end="")
+            id = create_alert(ws, CHEERS_SCENE, row["Source"], Path(row["File"]), loop=parse_bool(row["Repeat"]))
+
+            x_loc = int(row["X"]) if row["X"] else 0
+            y_loc = int(row["Y"]) if row["Y"] else 0
+            scale = float(row["Scale"]) if row["Scale"] else 1.0
+
+            if parse_bool(row["Needs Offset"]):
+                y_loc = y_loc - 50
+
+            # set the location, if there's one requested, and we just created it
+            if id is not None:
+                xform = {
+                    "positionX": x_loc,
+                    "positionY": y_loc,
+                    "scaleX": scale,
+                    "scaleY": scale,
+                }
+                set_source_transform(ws, CHEERS_SCENE, id, xform)
+
+            set_monitor_only(ws, row["Source"])
+
+    print("\nDONE!")
+    print(f"{counters['ok']} created, {counters['exists']} skipped (existing), {counters['missing']} missing video files, {counters['failure']} failures")
+
+    print("Press any key to exit")
+    msvcrt.getch()
+
+    ws.disconnect()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(f"\n\n!!  ERROR: SCRIPT FAILED\n\nexception: {e}", file=sys.stderr)
+        print("\nPress any key to exit")
+        msvcrt.getch()
+
+
+
+# useful tidbits when I was developing, keeping around in case they're useful:
+
+# zot5 = ws.call(requests.CreateInput(sceneName="CHEERS", inputName="test input",
+# inputKind = "ffmpeg_source", sceneItemEnabled = False, inputSettings = input_cfg))
+#
+# zot = ws.call(requests.GetSceneItemList(sceneName="CHEERS"))
+# zot2 = zot.getSceneItems()
+# print(ppretty(zot2))
+
+# zot3 = ws.call(requests.GetInputSettings(inputName="test input")).getInputSettings()
+# print(zot3)
+
+# zot4 = ws.call(requests.GetInputDefaultSettings(inputKind="ffmpeg_source"))
+# print(zot4)
+
+# zot5 = ws.call(requests.CreateInput(sceneName="CHEERS", inputName="test input",
+#                inputKind="ffmpeg_source", sceneItemEnabled=False, inputSettings=input_cfg))
+# print(zot5)
+# print(zot5.status)
+# print(zot5.getSceneItemId())
+# # scenes = ws.call(requests.GetSceneList()).getScenes()
+# # print(scenes)
+
+# # for s in scenes:
+# #     name = s['sceneName']
+# #     print(f"scene: {name}")
+# #     print(s)
---
+++ makesources/makesources.spec
@@ -0,0 +1,47 @@
+block_cipher = None
+
+a = Analysis(
+    ['makesources.py'],
+    pathex=[],
+    binaries=[
+        # ("ffmpeg.exe", "."),
+        # ("ffprobe.exe", ".")
+    ],
+    datas=[
+    #     ("lufsplot_icon.png", "lufsplot_icon.png")
+    ],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    cipher=block_cipher,
+    noarchive=False,
+)
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
+    [],
+    name='makesources',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=False,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=True,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+    # icon="lufsplot_icon.png",
+    # version="file_version_info.txt",
+)
